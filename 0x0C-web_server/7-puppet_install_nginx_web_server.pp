c2ba3108dd0cecebe27501e360fecf408fcc1479
Olaoluwa Babalola <lalaboiydvibeplug@gmail.com>
2023-09-11 06:58:03 -0700

    puppet configuration

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,9 +6,9 @@ package { 'nginx':
 
 file_line { 'install':
   ensure => 'present',
-  path => '/etc/nginx/sites-enabled/default',
-  after => 'listen 80 default_server;',
-  line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
@@ -17,5 +17,5 @@ file { '/var/www/html/index.html':
 
 service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
+  require => Package['nginx'],
 }
