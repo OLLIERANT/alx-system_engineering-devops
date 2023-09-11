ed38c60c01ff06d36ae9f7816fdcb320e0e60e02
Olaoluwa Babalola <lalaboiydvibeplug@gmail.com>
2023-09-11 06:52:53 -0700

    puppet configuration

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,11 +8,11 @@ file_line { 'install':
   ensure => 'present',
   path => '/etc/nginx/sites-enabled/default',
   after => 'listen 80 default_server;',
-  line => 'rewrite ^/redirect_me https://www.github.com/OLLIERANT permanent;',
+  line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World',
+  content => 'Hello World!',
 }
 
 service { 'nginx':
