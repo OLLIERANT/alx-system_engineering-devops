efdeb66dde957a35abac051e3a8a9e2095c55595
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2024-02-08 05:54:20 -0800

    subscribers number

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,9 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': '0x16-api_advanced:project:\
             v1.0.0 (by /u/firdaus_cartoon_jr)'}
+
+    if subreddit is None or type(subreddit) is not str:
+        return 0
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
